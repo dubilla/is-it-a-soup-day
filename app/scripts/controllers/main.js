c8564d07a6afc63048a9298d6f3b2f3a614c00db
Dan Ubilla <dan.ubilla@gmail.com>
2014-03-02 17:51:28 -0500

    Add conditionals to cover Friday, Saturday, and Sunday

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('isItAsoupDayApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.isit = 'Nah';
+  .controller('MainCtrl', function ($scope, $filter) {
+    var isit = 'Who knows?',
+        reason = '',
+        day = $filter('date')(new Date(), 'EEE').toLowerCase(),
+        isItTheWeekend;
+
+    isItTheWeekend = function(day) {
+      return day === 'sat' || day === 'sun';
+    };
+
+    if (day === 'fri') {
+      isit = 'Nah';
+      reason = 'Gotta get some good food with the whole crew!';
+    } else if (isItTheWeekend(day)) {
+      isit = 'Nah';
+      reason = 'You\'re not at work, right?';
+    }
+
+    $scope.isit = isit;
+    $scope.reason = reason;
   });
